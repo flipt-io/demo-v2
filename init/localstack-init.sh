b06eb9fe6fe10c873d48c68626b4e578699a06da
Sergei Pozniak <sergei.pozniak@insly.com>
2026-06-19 15:27:23 +0200

    Make init more stable with repo check

---
+++ init/localstack-init.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+set -e
+
+ENDPOINT=http://localstack:4566
+BUCKET=git-repos
+
+echo "Checking if S3 bucket already exists..."
+if aws --endpoint-url=$ENDPOINT s3 ls s3://$BUCKET 2>/dev/null; then
+  echo "Bucket already exists, skipping."
+  exit 0
+fi
+
+echo "Creating S3 bucket..."
+aws --endpoint-url=$ENDPOINT s3 mb s3://$BUCKET
+echo "Done."
---
+++ init/ogit-init.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+set -e
+
+echo "Checking if repo already exists..."
+if curl -sf http://ogit:8080/onoffinc-features.git/info/refs > /dev/null; then
+  echo "Repo already exists, skipping."
+  exit 0
+fi
+
+echo "Creating repo..."
+curl -sf -X POST http://ogit:8080/api/repo \
+  -H 'Content-Type: application/json' \
+  -d '{"name": "onoffinc-features"}' || true
+
+echo "Cloning and pushing initial content..."
+git clone http://ogit:8080/onoffinc-features.git /tmp/repo
+cd /tmp/repo
+git config user.email 'init@local'
+git config user.name 'init'
+mkdir -p default
+cp /init/default-features.yaml ./default/features.yaml
+git add .
+git commit -m 'initial commit'
+git push origin main
+
+echo "Done."
